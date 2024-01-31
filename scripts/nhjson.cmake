5aa8b3eec7e87c319d485d82191354b51df03d56
SpaghettDev <37266659+SpaghettDev@users.noreply.github.com>
2024-01-31 15:29:20 -0600

    Fix nlohmann-json cmake script (thanks matjson for this error again 👍)

--- scripts/nhjson.cmake
+++ scripts/nhjson.cmake
@@ -2,11 +2,12 @@ include(FetchContent)
 
 set(JSON_MultipleHeaders OFF)
 
+# deprecated, move to nlohmann-json releases
 FetchContent_Declare(
-    json
+    nhjson
     GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent.git
-    GIT_TAG        67e6070f9d9a44b4dec79ebe6b591f39d2285593
+    GIT_TAG        326308d7512d2168ae00199aec4dd0f714526e89
     GIT_PROGRESS TRUE
 )
 message("Fetching nlohmann-json")
-FetchContent_MakeAvailable(json)
+FetchContent_MakeAvailable(nhjson)
