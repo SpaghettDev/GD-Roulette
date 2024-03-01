2f4809e4e2bdfc801171b2645966d5e287597716
SpaghettDev <37266659+SpaghettDev@users.noreply.github.com>
2024-03-01 16:21:42 -0600

    Bump rtrp version

--- cmake/rtrp.cmake
+++ cmake/rtrp.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     RTRP
     GIT_REPOSITORY https://github.com/SpaghettDev/RTRP.git
-    GIT_TAG        2a9d23dcc05483256f5a963356e927c11ec9ffdb
+    GIT_TAG        53fe19f867b4b52b241f4c0522299896cb32c628
     GIT_PROGRESS TRUE
 )
 message("Fetching RTRP")
