7148ceab79b901a0649c6ff9e3b2a1c4e5de07e2
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2024-06-20 20:50:52 -0400

    Big Button Toggle and Fixes (#24)
    * Big Button Toggle and Fixes
    
    * Fix oversight

--- src/layers/CreatorLayer.cpp
+++ src/layers/CreatorLayer.cpp
@@ -20,24 +20,34 @@ class $modify(RouletteButton, CreatorLayer)
 
 		auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-		auto menu = CCMenu::create();
-		menu->setPosition({ 24.f, winSize.height / 2 });
-		menu->setID("center-left-menu"_spr);
-		this->addChild(menu);
+		if(Mod::get()->getSettingValue<bool>("use-big-button") && Loader::get()->isModLoaded("alphalaneous.pages_api")) {
+			if(auto menu = getChildByID("creator-buttons-menu")) {
+				auto spr = CCSprite::create("RL_btn_big.png"_spr);
+				spr->setScale(0.842);
+				auto rouletteButton = CCMenuItemSpriteExtra::create(
+					spr,
+					this,
+					menu_selector(RouletteButton::onRouletteButton)
+				);
+				rouletteButton->setID("roulette-button"_spr);
+				menu->addChild(rouletteButton);
+			}
+		}
+		else {
+			if(auto menu = getChildByID("exit-menu")) {
+				auto spr = CircleButtonSprite::createWithSprite("RL_btn_001.png"_spr, 1.0f, CircleBaseColor::Green, CircleBaseSize::Small);
 
-		// this still sucks :(
-		auto rouletteButton = CCMenuItemSpriteExtra::create(
-			CCSprite::create("RL_blankBtn_001.png"_spr),
-			this,
-			menu_selector(RouletteButton::onRouletteButton)
-		);
-		rouletteButton->setID("roulette-button"_spr);
-		auto rouletteSprite = CCSprite::create("RL_btn_001.png"_spr);
-		rouletteSprite->setScale(.725f);
-		rouletteSprite->setPosition({ 22.5f, 23.75f });
-		rouletteButton->addChild(rouletteSprite);
-
-		menu->addChild(rouletteButton);
+				auto rouletteButton = CCMenuItemSpriteExtra::create(
+					spr,
+					this,
+					menu_selector(RouletteButton::onRouletteButton)
+				);
+				rouletteButton->setID("roulette-button"_spr);
+				menu->addChild(rouletteButton);
+				typeinfo_cast<AxisLayout*>(menu->getLayout())->setAxisReverse(true);
+				menu->updateLayout();
+			}
+		}
 
 		return true;
 	}
